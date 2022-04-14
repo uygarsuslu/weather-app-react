d17e10b6540dd9bddd7a39e5ae3c0eccdcf5ba3b
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-14 20:29:10 +0300

    page added

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import Header from '../Header';
+
+import styles from './Page.module.css';
+
+import Form from '../Form';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
+
+import useForecast from '../../hooks/useForecast';
+
+const Page = () => {
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
+
+    const onSubmit = value => {
+        // call in here
+        submitRequest(value);
+    };
+
+    return (
+        <>
+            <Fragment>
+                <Header />
+                {!forecast && (
+                    <div className={`${styles.box} position-relative`}>
+                        {/* Form */}
+                        {!isLoading && <Form submitSearch={onSubmit} />}
+                        {/* Error */}
+                        {isError && <Error message={isError} />}
+                        {/* Loader */}
+                        {isLoading && <Loader />}
+                    </div>
+                )}
+                {/* Forecast */}
+                {forecast && <Forecast forecast={forecast} />}
+            </Fragment>
+        </>
+    );
+};
+
+export default Page;
