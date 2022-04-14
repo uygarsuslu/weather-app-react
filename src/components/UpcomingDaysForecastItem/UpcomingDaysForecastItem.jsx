66a0c77089cd67bb64fc1f29fa780184d98c507a
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-14 20:28:51 +0300

    upcoming days forecast item added

---
+++ src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './UpcomingDaysForecastItem.module.css';
+
+const imgUrlBase = 'https://www.metaweather.com/static/';
+
+const UpcomingDaysForecastItem = ({ weekday, temperature, imgUrl }) => (
+    <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}>
+        <img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${imgUrl}.svg`} alt="" />
+        <span className="mb-2">{weekday}</span>
+        <span className="font-weight-bold">{temperature}&deg;C</span>
+    </li>
+);
+
+export default UpcomingDaysForecastItem;
